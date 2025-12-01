75cc89bf5ea059dda78316bced6f1e092bb38eb9
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-01 16:11:02 -0300

    nextjs gerenciamento escolar: traduz card label para português

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,6 +1,12 @@
 import prisma from "@/lib/prisma";
 import Image from "next/image";
 
+const typeLabels: Record<string, string>={ 
+  admin: "Administradores",
+  teacher: "Professores",
+  student: "Alunos",
+  parent: "Responsáveis",
+}
 const UserCard = async ({
   type,
 }: {
@@ -19,12 +25,12 @@ const UserCard = async ({
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
       <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
-          2024/25
+           {new Date().getFullYear()}/{(new Date().getFullYear() + 1).toString().slice(-2)}
         </span>
         <Image src="/more.png" alt="" width={20} height={20} />
       </div>
       <h1 className="text-2xl font-semibold my-4">{data}</h1>
-      <h2 className="capitalize text-sm font-medium text-gray-500">{type}s</h2>
+      <h2 className="capitalize text-sm font-medium text-gray-500">{typeLabels[type]}</h2>
     </div>
   );
 };
