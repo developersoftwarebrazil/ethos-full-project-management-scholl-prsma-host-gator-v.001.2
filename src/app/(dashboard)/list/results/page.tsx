a9dd337249deb43af08a1edb00c841580f1dfd43
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-01 18:53:32 -0300

    nextjs gerenciamento escolar: modifica o metodo de mostrar nome para uma template string

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -21,84 +21,81 @@ type ResultList = {
   startTime: Date;
 };
 
-
 const ResultListPage = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) => {
+  const { userId, sessionClaims } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
 
-const { userId, sessionClaims } = auth();
-const role = (sessionClaims?.metadata as { role?: string })?.role;
-const currentUserId = userId;
+  const columns = [
+    {
+      header: "Título",
+      accessor: "title",
+    },
+    {
+      header: "Aluno",
+      accessor: "student",
+    },
+    {
+      header: "Nota",
+      accessor: "score",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Professor",
+      accessor: "teacher",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Turma",
+      accessor: "class",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Data",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin" || role === "teacher"
+      ? [
+          {
+            header: "Ações",
+            accessor: "action",
+          },
+        ]
+      : []),
+  ];
 
-
-const columns = [
-  {
-    header: "Título",
-    accessor: "title",
-  },
-  {
-    header: "Aluno",
-    accessor: "student",
-  },
-  {
-    header: "Nota",
-    accessor: "score",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Professor",
-    accessor: "teacher",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Turma",
-    accessor: "class",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Data",
-    accessor: "date",
-    className: "hidden md:table-cell",
-  },
-  ...(role === "admin" || role === "teacher"
-    ? [
-        {
-          header: "Ações",
-          accessor: "action",
-        },
-      ]
-    : []),
-];
-
-const renderRow = (item: ResultList) => (
-  <tr
-    key={item.id}
-    className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
-  >
-    <td className="flex items-center gap-4 p-4">{item.title}</td>
-    <td>{item.studentName + " " + item.studentName}</td>
-    <td className="hidden md:table-cell">{item.score}</td>
-    <td className="hidden md:table-cell">
-      {item.teacherName + " " + item.teacherSurname}
-    </td>
-    <td className="hidden md:table-cell">{item.className}</td>
-    <td className="hidden md:table-cell">
-      {new Intl.DateTimeFormat("en-US").format(item.startTime)}
-    </td>
-    <td>
-      <div className="flex items-center gap-2">
-        {(role === "admin" || role === "teacher") && (
-          <>
-            <FormContainer table="result" type="update" data={item} />
-            <FormContainer table="result" type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
+  const renderRow = (item: ResultList) => (
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td>{`${item.studentName} ${item.studentSurname}`}</td>
+      <td className="hidden md:table-cell">{item.score}</td>
+      <td className="hidden md:table-cell">
+        {`${item.teacherName} |${item.teacherSurname}`}
+      </td>
+      <td className="hidden md:table-cell">{item.className}</td>
+      <td className="hidden md:table-cell">
+        {new Intl.DateTimeFormat("en-US").format(item.startTime)}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {(role === "admin" || role === "teacher") && (
+            <>
+              <FormContainer table="result" type="update" data={item} />
+              <FormContainer table="result" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   const { page, ...queryParams } = searchParams;
 
@@ -209,7 +206,9 @@ const renderRow = (item: ResultList) => (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">Todos os Resultados</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          Todos os Resultados
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
