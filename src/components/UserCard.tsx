ccd87c3658d7922dcd03a9345634551cdb41b892
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-03 20:29:38 -0300

    nextjs gerenciamento escolar: conserto do bug no card admin

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,31 +1,48 @@
 import prisma from "@/lib/prisma";
 import Image from "next/image";
+import { clerkClient } from "@clerk/nextjs/server";
 
-const typeLabels: Record<string, string>={ 
+const typeLabels = { 
   admin: "Administradores",
   teacher: "Professores",
   student: "Alunos",
   parent: "Responsáveis",
-}
-const UserCard = async ({
-  type,
-}: {
-  type: "admin"  | "teacher" | "student" | "parent";
-}) => {
-  const modelMap: Record<typeof type, any> = {
-    admin: prisma.admin,
-    teacher: prisma.teacher,
-    student: prisma.student,
-    parent: prisma.parent,
-  };
+};
 
-  const data = await modelMap[type].count();
+const UserCard = async ({ type }: {
+  type: "admin" | "teacher" | "student" | "parent";
+}) => {
+
+  let data = 0;
+
+  // ADMIN VIA CLERK
+  if (type === "admin") {
+    const users = await clerkClient.users.getUserList();
+    data = users.data.filter(
+      (u) => u.publicMetadata.role === "admin"
+    ).length;
+  }
+
+  // PRISMA: TEACHER
+  if (type === "teacher") {
+    data = await prisma.teacher.count();
+  }
+
+  // PRISMA: STUDENT
+  if (type === "student") {
+    data = await prisma.student.count();
+  }
+
+  // PRISMA: PARENT
+  if (type === "parent") {
+    data = await prisma.parent.count();
+  }
 
   return (
     <div className="rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1 min-w-[130px]">
       <div className="flex justify-between items-center">
         <span className="text-[10px] bg-white px-2 py-1 rounded-full text-green-600">
-           {new Date().getFullYear()}/{(new Date().getFullYear() + 1).toString().slice(-2)}
+          {new Date().getFullYear()}/{(new Date().getFullYear() + 1).toString().slice(-2)}
         </span>
         <Image src="/more.png" alt="" width={20} height={20} />
       </div>
