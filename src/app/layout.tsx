a7dee29559898a31e56b261aa5dafb3e2cc32af9
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2025-12-20 00:12:03 -0300

    nextjs gerenciamento escolar-deploy: fix: build font wolf2.v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,37 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
-
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import localFont from "next/font/local";
 
 const inter = localFont({
   src: [
-    { path: "../fonts/Inter_18pt-Regular.woff2", weight: "400" },
+    { path: "../fonts/Inter-Regular.woff2", weight: "400" },
     { path: "../fonts/Inter-Medium.woff2", weight: "500" },
-    { path: "../fonts/Inter_24pt-SemiBold.woff2", weight: "600" },
-    { path: "../fonts/Inter_18pt-Bold.woff2", weight: "800" },
+    { path: "../fonts/Inter-SemiBold.woff2", weight: "600" },
+    { path: "../fonts/Inter-Bold.woff2", weight: "700" },
   ],
+  variable: "--font-inter",
   display: "swap",
 });
 
 export const metadata: Metadata = {
-  title: "Plataforma Ethos CPAC Escola de Psicologia",
+  title: "Plataforma Ethos CPAC Escola de Psícologia",
   description: "Ethos CPAC Escola de psicologia",
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <ClerkProvider>
-      <html lang="pt-BR">
+      <html lang="en">
         <body className={inter.className}>
-          {children}
-          <ToastContainer position="bottom-right" theme="dark" />
+          {children} <ToastContainer position="bottom-right" theme="dark" />
         </body>
       </html>
     </ClerkProvider>
