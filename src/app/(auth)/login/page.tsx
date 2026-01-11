ec6fe04d334db2111f85800665fe8ebf22a5f878
developersoftwarebrazil <contatodevelopersbrazildna@gmail.com>
2026-01-11 13:09:18 -0300

    nextjs gerenciamento escolar-deploy:refactor structure of logiregister page.v1

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -17,7 +17,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-import styles from "@/styles/account/Login.module.scss";
+import styles from "@/styles/account/login/Login.module.scss";
 
 
 const LoginPage = () => {
